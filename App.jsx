53599af8fb43cc476dbf5e6b7c6dfbeefad5d208
Madina <madinajama9@gmail.com>
2022-12-09 06:10:10 -0500

    newly added js

--- App.jsx
+++ App.jsx
@@ -1,11 +1,20 @@
 import './App.css';
-import {BrowserRouter, Routes, Route}
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact';
+import Nav from './components/Nav'
+import Users from "./pages/Users"
 
 function App() {
 
   return (
-    <div>
-    </div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/users/:id' element={<Users />} />
+        </Routes>
+      </Router>
   );
 }
 
